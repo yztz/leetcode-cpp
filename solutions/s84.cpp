643a33126ed2c1620a8f0a5c5b47ec864a3fd9ed
yztz <781089956@qq.com>
2026-01-20 00:19:04 +0800

    s84: 72. 编辑距离

---
+++ solutions/s84.cpp
@@ -0,0 +1,39 @@
+#include "lib.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[1][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                }
+            }
+         }
+         return dp[m][n];
+    }
+};
+
+int main() {
+    Solution s;
+    vector v = {15,8,19};
+    vector v2 = {38,36,23};
+
+    return 0;
+}
