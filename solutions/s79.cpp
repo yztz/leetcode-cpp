e6dbe4594c5d6d78301b7959d180ce78ca07a0fc
yztz <781089956@qq.com>
2026-01-19 18:22:14 +0800

    s79: 236. 二叉树的最近公共祖先

---
+++ solutions/s79.cpp
@@ -0,0 +1,26 @@
+#include "lib.h"
+
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return nullptr;
+        if (root == p) return p;
+        if (root == q) return q;
+
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+
+        if (l && r) return root;
+        return l ? l : r;
+    }
+};
+
+int main() {
+    Solution s;
+    vector v = {15,8,19};
+    vector v2 = {38,36,23};
+
+    return 0;
+}
