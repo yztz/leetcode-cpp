2e2d490c026357ea0aca23ad7a898628afe8c210
yztz <781089956@qq.com>
2026-01-22 22:40:11 +0800

    104. 二叉树的最大深度

---
+++ solutions/s103.cpp
@@ -0,0 +1,19 @@
+#include "lib.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+int main() {
+    Solution s;
+    vector v = {15,8,19};
+    vector v2 = {38,36,23};
+
+    return 0;
+}
